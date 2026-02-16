c24b59e127517226f703e9cfc5f278df43211295
jan-sommerfeldt <127450373+jan-sommerfeldt@users.noreply.github.com>
2026-02-16 18:31:17 +0100

    Update CONSTANTS.py

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -5,51 +5,51 @@ dataVolume = 2.5  # Tb/day
 #RF Parameters:
 
 # Geometry / frequency
-f_hz = 20e9
+f_hz = 1
 # Carrier frequency [Hz]
 
-R_m = 1200e3
+R_m = 1
 # Slant range between spacecraft and ground station [m]
 
 # Transmit chain
-P_tx_dBW = 10.0
+P_tx_dBW = 1
 # Transmit RF power at HPA output [dBW]
 
-G_tx_dBi = 30.0
+G_tx_dBi = 1
 # Transmit antenna gain [dBi]
 
-L_tx_dB = 1.0
+L_tx_dB = 1
 # Transmit-side losses [dB]
 
-L_point_tx_dB = 0.0
+L_point_tx_dB = 1
 # Transmit pointing loss [dB]
 
-# Propagation losses (benchmark)
-L_atm_dB = 0.5
+# Propagation losses
+L_atm_dB = 1
 # Atmospheric/gaseous attenuation [dB]
 
-L_rain_dB = 0.0
+L_rain_dB = 1
 # Rain attenuation [dB]
 
-L_pol_dB = 0.0
+L_pol_dB = 1
 # Polarization mismatch loss [dB]
 
-L_point_rx_dB = 0.0
+L_point_rx_dB = 1
 # Receive pointing loss [dB]
 
-L_misc_dB = 0.0
+L_misc_dB = 1
 # Miscellaneous additional losses/margins in path [dB]
 
 # Receiver
-G_rx_dBi = 55.0
+G_rx_dBi = 1
 # Receive antenna gain [dBi]
 
-T_sys_K = 500.0
+T_sys_K = 1
 # System noise temperature at antenna terminals [K]
 
 # Waveform / throughput
 B_occ_hz = 100e6
-# Occupied (null-to-null) bandwidth used in design [Hz]
+# Occupied (null-to-null) bandwidth [Hz]
 
 alpha = 0.2
 # Roll-off factor
@@ -61,5 +61,5 @@ M = 4
 BER_target = 1e-6
 # Target bit error rate
 
-L_impl_dB = 0.0
+L_impl_dB = 0
 # Implementation loss applied at Eb/N0 level [dB]
